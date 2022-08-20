abdb16d2c65f95c9f83d23996ef4ba2918ba0de4
palapalado <ms0529756@gmail.com>
2022-08-20 15:09:22 +0000

    init frontend static pages

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # aws-serverless-auth-management
 
 # build image & run container
+
 ```
 docker-compose --build -d
+```
+
+# build frontend static resources
+
+```
+cd frontend
+pnpm i
+pnpm build
+```
+
+# deploy serverless resources
+
+```
+cd authmgmt
+sls deploy --profile serverless-agent]
+```
+
+# upload frontend static files to s3
+
+```
+aws --profile serverless-agent s3 sync --delete ./frontend/build s3://palapalado.tk/
 ```
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import routes from '@/routes'
-import { FormControl, Grid, Box } from '@mui/material';
-import SignIn from '@/pages/SignIn'
 
 
 const App = () => {
+  useEffect(() => {
+    window._config = {
+      cognito: {
+        userPoolId: 'us-east-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+      },
+      api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+      }
+    };
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
